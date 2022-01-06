b4bb50ebbc41a14c81b294b7fd3863935ee52ccb
lorenzopepe98 <lore.pepe998@gmail.com>
2022-01-06 16:13:36 +0100

    multi page config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-export default {
+// vite.config.js
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				01: resolve(__dirname, "01/index.html"),
+				02: resolve(__dirname, "02/index.html"),
+				03: resolve(__dirname, "03/index.html"),
+			},
+		},
+	},
 	server: {
 		host: true,
 	},
-};
+});
