902ca3ecb38eeab3b8113d3d2cf5997aa2757dcc
lorenzopepe98 <lore.pepe998@gmail.com>
2022-01-07 17:54:29 +0100

    04

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ module.exports = defineConfig({
 				01: resolve(__dirname, "01/index.html"),
 				02: resolve(__dirname, "02/index.html"),
 				03: resolve(__dirname, "03/index.html"),
+				04: resolve(__dirname, "04/index.html"),
 			},
 		},
 	},
